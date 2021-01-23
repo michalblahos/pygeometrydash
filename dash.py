30f64ff07b9f764e63483c72d1bb2e5a60e8440b
Petr Blahoš <petr@blahos.com>
2021-01-23 11:09:12 +0100

    Udělaná struktura, velké zjednodušení
    Překážky jsou zvláštní objekty. A to je teprve začátek.

--- dash.py
+++ dash.py
@@ -10,6 +10,37 @@ WIDTH = 1400
 HEIGHT = 1000
 
 
+class Podlaha(Actor):
+    def __init__(self, x):
+        super().__init__("prekazka", pos=(x*50, 500))
+
+
+
+class Prekazka(Actor):
+    def step(self):
+        self.x -= 5
+
+class Blok(Prekazka):
+    def __init__(self, y):
+        super().__init__("block", midtop=(1450, 50*y))
+
+class Spike(Prekazka):
+    def __init__(self, y):
+        super().__init__("bodak", midbottom=(1450, 50+50*y))
+
+class Booster(Prekazka):
+    def __init__(self, y):
+        super().__init__("booster", midbottom=(1450, 50+50*y))
+
+class Skakmed(Prekazka):
+    def __init__(self, y):
+        super().__init__("skakadlo-medium", midbottom=(1450, 50+50*y))
+
+    def step(self):
+        super().step()
+        self.angle -= 10
+
+
 class Hra:
     def __init__(self):
         self.run = False
@@ -18,7 +49,7 @@ class Hra:
         self.jelito.pada = True
         self.injump = False
         self.init()
-        
+
     def init(self):
         self.stred = 500
         self.stred_smer = 0
@@ -26,56 +57,39 @@ class Hra:
         self.body = 0
         self.level = LevelImage("images/level3.png")
         self.level.reset()
-        self.blocks = []
         self.podlaha = []
-        self.spike = []
-        self.boosters = []
+        self.prekazky = []
+        
         self.jelito.pos = (100, 850)
         self.jelito.smer_x = 0
         self.jelito.smer_y = 0
-        self.skakmed = []
         for i in range(30):
-            plomba = Actor("prekazka", pos=(i*50, 500))
-            self.podlaha.append(plomba)
+            self.podlaha.append(Podlaha(i))
 
         self.run = True
 
     def draw(self):
         for i in self.podlaha:
             i.draw()
-        for i in self.spike:
-            i.draw()
-        for i in self.blocks:
-            i.draw()
-        for i in self.boosters:
-            i.draw()
-        for i in self.skakmed:
+        for i in self.prekazky:
             i.draw()
         self.jelito.draw()
 
 
     def update(self):
-        if len(self.spike) > 0 and self.spike[0].x <= -50:
-            del self.spike[0] 
-        if len(self.boosters) > 0 and self.boosters[0].x <= -50:
-            del self.boosters[0] 
-        if len(self.skakmed) > 0 and self.skakmed[0].x <= -50:
-            del self.skakmed[0]
-        for i in self.spike:
-            i.x -= 5
+        del_cnt = 0
+        while len(self.prekazky) > 0 and self.prekazky[0].x <= -50:
+            del self.prekazky[0]
+            del_cnt += 1
+        if (del_cnt): print(del_cnt)
 
         for i in self.podlaha:
             i.x -= 5
             if i.x <= -50:
                 i.x = 1450
-        for i in self.boosters:
-            i.x -= 5
-        for i in self.skakmed:
-            i.x -= 5
-            i.angle += -10
-        self.blocks = [i for i in self.blocks if i.x > -50]
-        for i in self.blocks:
-            i.x -= 5
+
+        for i in self.prekazky:
+            i.step()
 
         self.naraz()
         self.update_jelito()
@@ -87,17 +101,13 @@ class Hra:
             sloupec = self.level.get_next()
             for (typ, y) in sloupec:
                 if typ == "blok":
-                    bl = Actor("block", midtop=(1450, 50*y))
-                    self.blocks.append(bl)
+                    self.prekazky.append(Blok(y))
                 if typ == "spike":
-                    s = Actor("bodak", midbottom=(1450, 50*y+50))
-                    self.spike.append(s)
+                    self.prekazky.append(Spike(y))
                 if typ == "booster-1":
-                    s = Actor("booster", midbottom=(1450, 50*y+50))
-                    self.boosters.append(s)
+                    self.prekazky.append(Booster(y))
                 if typ == "skakmed":
-                    s = Actor("skakadlo-medium", midbottom=(1450, 50*y+50))
-                    self.skakmed.append(s)
+                    self.prekazky.append(Skakmed(y))
     def jump(self):
         if self.jelito.up <= 0 and not self.jelito.pada:
             self.jelito.up = 100
@@ -107,7 +117,7 @@ class Hra:
         if self.jelito.up > 0:
             self.jelito.y -= 5
             self.jelito.up -= 5
-        else:   
+        else:
             dolu = self.o_kolik_dolu()
             if dolu > 0:
                 if dolu > 5:
@@ -119,50 +129,54 @@ class Hra:
                 self.jelito.pada = False
                 if self.injump:
                     self.jump() 
+        # TODO: strcit boost do naraz
         self.boost()
 
     def boost(self):
-        for i in self.boosters:
+        for i in self.prekazky:
             if self.jelito.right >= i.left and self.jelito.right <= i.right:
-                if self.jelito.colliderect(i):
-                    print(">>>>>> trefa jo?")
-                    self.jelito.up = 200
-        
+                if isinstance(i, Booster):
+                    if self.jelito.colliderect(i):
+                        print(">>>>>> trefa jo?")
+                        self.jelito.up = 180
+            
     def naraz(self):
-        for i in self.blocks:
+        for i in self.prekazky:
+            if i.left > self.jelito.right:
+                break
             if self.jelito.right >= i.left and self.jelito.right <= i.right:
                 if self.jelito.colliderect(i):
-                    if self.jelito.bottom <= i.top:
-                        pass
-                    else:
+                    if isinstance(i, Blok):
+                        if self.jelito.bottom <= i.top:
+                            pass
+                        else:
+                            self.kill()
+                    if isinstance(i, Spike):
+                        print(f"Kill se spike {i.left} {i.right}")
+                        self.jelito.image = "zelenej"
                         self.kill()
-        for i in self.spike:
-            if self.jelito.right >= i.left and self.jelito.right <= i.right:
-                if self.jelito.colliderect(i):
-                    print(f"Kill se spike {i.left} {i.right}")
-                    self.jelito.image = "zelenej"
-                    self.kill()
-                else:
-                    print(f"--- No-kill se spike {i.left} {i.right}")
-                    self.jelito.image = "player"
-        
+
     def kill(self):
         print("kill")
         self.run = False
+        self.jelito.image = "zelenej"
 
     def o_kolik_dolu(self):
         dolu = 0
         dolu2 = -1
         if self.jelito.bottom < 950:
             dolu = 950 - self.jelito.bottom
-        for i in self.blocks:
-            if i.x >= self.jelito.x - 25 and i.x <= self.jelito.x + 25:
-                if i.top == self.jelito.bottom:
-                    return 0
-                if i.top > self.jelito.bottom:
-                    d = i.top - self.jelito.bottom
-                    if d < dolu2 or dolu2 == -1:
-                        dolu2 = d
+        for i in self.prekazky:
+            if i.left > self.jelito.right:
+                break
+            if isinstance(i, Blok):
+                if i.left <= self.jelito.right and i.right >= self.jelito.left:
+                    if i.top == self.jelito.bottom:
+                        return 0
+                    if i.top > self.jelito.bottom:
+                        d = i.top - self.jelito.bottom
+                        if d < dolu2 or dolu2 == -1:
+                            dolu2 = d
         if dolu2 > 0:
             return dolu2
         return dolu    
